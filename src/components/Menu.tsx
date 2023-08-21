1ae38db7938f2f4e8c8c0d0610565aefd3775ab5
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-08-20 20:13:18 -0400

    text writer animation, and animation under navlinks

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,9 +2,11 @@ const Menu = () => {
   return (
     <nav className="hidden md:flex md:justify-center">
       <ul className="flex flex-col  md:flex-row gap-4 text-lg">
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li className="nav-links">About</li>
+        <li>|</li>
+        <li className="nav-links">Projects</li>
+        <li>|</li>
+        <li className="nav-links">Contact</li>
       </ul>
     </nav>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="sticky top-0 z-40 w-full h-16 lg:border-b lg:border-b-slate-500/10 bg-transparent shadow-slate-50">
       <header className="container grid grid-cols-3 navbar h-full items-center mx-auto ">
-        <h1 className="text-2xl pl-2 md:pl-1 font-serif">MarioSergDC</h1>
+        <h1 className="brand text-2xl pl-2 md:pl-1 font-serif">MarioSergDC</h1>
         <Menu />
       </header>
     </div>
