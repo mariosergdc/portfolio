0602fa27cf2de75e9458f0d2c8fa8579cc0ef52a
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-08-25 11:37:52 -0400

    section home,html scrool smooth,header max w lg

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,35 @@ import NavBar from "./components/NavBar";
 export default function App() {
   return (
     <div
-      className="relative flex items-center min-h-screen flex-col
+      className="app relative flex items-center min-h-screen flex-col
     bg-gradient-to-tr from-sky-800 via-transparent via-80% to-white/[0.9]"
     >
       <NavBar />
       <main className="main w-full">
         <section
-          id="section-1"
-          className="section-1 flex content-center justify-center flex-wrap
-          bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
+          id="home"
+          className="home flex content-center justify-center flex-wrap
+          bg-gradient-to-tr from-sky-900 via-transparent via-80% to-white/[0.9]"
         >
           <div>Imagen de set up hero</div>
         </section>
         <section
+          id="section-1"
+          className="section-1 flex content-center justify-center flex-wrap
+          bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
+        >
+          <div>About</div>
+        </section>
+        <section
           id="section-2"
-          className="section-2
+          className="section-2 flex content-center justify-center flex-wrap
         bg-gradient-to-tr from-sky-900 via-transparent via-80% to-white/[0.9]"
         >
           eeee
         </section>
         <section
           id="section-3"
-          className="section-3
+          className="section-3 flex content-center justify-center flex-wrap
         bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
         >
           q
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,10 @@ const Menu = () => {
     <nav className="hidden md:flex md:justify-center">
       <ul className="flex flex-col  md:flex-row gap-4 text-lg">
         <li className="nav-links">
+          <a href="#home">Home</a>
+        </li>
+        <li>|</li>
+        <li className="nav-links">
           <a href="#section-1">About</a>
         </li>
         <li>|</li>
