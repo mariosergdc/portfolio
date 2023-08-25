d57387bc53dac6adc40a3db9a51eb349f62c4d6d
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-08-24 23:05:53 -0400

    navbar sticky,bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import NavBar from "./components/NavBar";
 export default function App() {
   return (
     <div
-      className="app relative flex items-center min-h-screen flex-col overflow-hidden
-    bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
+      className="relative flex items-center min-h-screen flex-col
+    bg-gradient-to-tr from-sky-800 via-transparent via-80% to-white/[0.9]"
     >
       <NavBar />
       <main className="main w-full">
         <section
           id="section-1"
-          className="section-1 bg-cyan-950 flex content-center justify-center flex-wrap"
+          className="section-1 flex content-center justify-center flex-wrap
+          bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
         >
           <div>Imagen de set up hero</div>
         </section>
-        <section id="section-2" className="section-2 bg-cyan-800">
+        <section
+          id="section-2"
+          className="section-2
+        bg-gradient-to-tr from-sky-900 via-transparent via-80% to-white/[0.9]"
+        >
           eeee
         </section>
-        <section id="section-3" className="section-3 bg-cyan-950">
+        <section
+          id="section-3"
+          className="section-3
+        bg-gradient-to-tr from-sky-950 via-transparent via-80% to-white/[0.9]"
+        >
           q
         </section>
       </main>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,10 @@ import Menu from "./Menu";
 
 const NavBar = () => {
   return (
-    <div className="sticky top-0 z-40 w-full h-16 lg:border-b lg:border-b-slate-500/10 bg-transparent shadow-slate-50">
+    <div
+      className="sticky top-0 z-40 w-full h-16 lg:border-b lg:border-b-slate-500/10 
+    bg-gradient-to-r from-sky-800 via-sky-700 via-80% to-white/[0.9] shadow-slate-50"
+    >
       <header className="container grid grid-cols-3 navbar h-full items-center mx-auto ">
         <h1 className="brand text-2xl pl-2 md:pl-1 font-serif">MarioSergDC</h1>
         <Menu />
