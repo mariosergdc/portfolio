b0b4cd0560e579c81cac8f7fe63c5f40ab9d2ff3
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-10-04 12:37:02 -0400

    change undel-link in css,use themecontext in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,37 @@
+import { useContext } from "react";
+import ThemeContext from "./context/ThemeProvider";
+
 const Menu = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <nav className="hidden md:flex md:justify-center">
       <ul className="flex flex-col  md:flex-row gap-4 text-lg">
         <li className="nav-links">
           <a href="#home">Home</a>
+          <div
+            className={`under-link ${theme === "dark" ? "light" : "dark"}`}
+          ></div>
         </li>
         <li>|</li>
         <li className="nav-links">
           <a href="#about">About</a>
+          <div
+            className={`under-link ${theme === "dark" ? "light" : "dark"}`}
+          ></div>
         </li>
         <li>|</li>
         <li className="nav-links">
           <a href="#projects">Projects</a>
+          <div
+            className={`under-link ${theme === "dark" ? "light" : "dark"}`}
+          ></div>
         </li>
         <li>|</li>
         <li className="nav-links">
           <a href="#contact">Contact</a>
+          <div
+            className={`under-link ${theme === "dark" ? "light" : "dark"}`}
+          ></div>
         </li>
       </ul>
     </nav>
